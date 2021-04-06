bcd3df890a1f97144c165b3d6ca96a9a93f59bd7
Timo <timo.haetinger@gmx.de>
2021-04-06 16:51:15 +0200

    - debugging

--- user_events/routes/file.js
+++ user_events/routes/file.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     if (req.body.id != null) {
+        console.log("Bis hier her gehts");
         File.findById(req.body.id, function (err, file) {
             if(!err){
                 const {_id: id, __v, file, user_id, ...rest} = file.toObject();
